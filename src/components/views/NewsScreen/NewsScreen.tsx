def4a6b98a626d16f57d5857930a3495982b435b
JoseFlorez <tiomix68@gmail.com>
2022-11-18 13:34:57 -0500

    Update NewsScreen.tsx

--- src/components/views/NewsScreen/NewsScreen.tsx
+++ src/components/views/NewsScreen/NewsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
@@ -17,11 +17,79 @@ import ImageNotFound from "../../../assets/image-not-found.png";
 import Chip from "@mui/material/Chip";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
-import { CardHeader } from "@mui/material";
+import { CardHeader, FormControl, TextField } from "@mui/material";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebase/firebase-config";
+import { useAppDispatch } from "../../../redux/hooks/hooks";
+import { setNotification } from "../../../redux/actions/ui";
 
 const NewsScreen = () => {
+  const dispatch = useAppDispatch();
+
   const { getListReportsQuery } = useListReportsQuery();
   const { searchNews, news } = getListReportsQuery();
+
+  const [solution, setSolution] = useState({
+    description: "",
+    image: "",
+    title: "",
+  });
+
+  function checkURL(url: string) {
+    return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
+  }
+
+  const handleChangeSolution = (
+    event: React.SyntheticEvent<Element, Event>
+  ) => {
+    const { name, value } = event.currentTarget as HTMLInputElement;
+    console.log("datos del imput team:", { name, value });
+    switch (name) {
+      case "description":
+        if (value !== "") {
+          console.log("La description asignado es:", value);
+          setSolution({ ...solution, description: value });
+        } else {
+          console.error("Erro en la description");
+        }
+        break;
+      case "image":
+        if (value !== "") {
+          if (checkURL(value)) {
+            console.log("La image asignado es:", value);
+            setSolution({ ...solution, image: value });
+          } else {
+            console.error("Erro en la image");
+          }
+        } else {
+          console.error("Erro en la image");
+        }
+        break;
+      case "titulo":
+        if (value !== "") {
+          console.log("El titulo asignado es:", value);
+          setSolution({ ...solution, title: value });
+        } else {
+          console.error("Erro en el titulo");
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClick = () => {
+    const newsReference = collection(db, "news");
+    addDoc(newsReference, { ...solution, state: 1 }).then((value)=>{
+      dispatch(
+        setNotification(
+          true,
+          "Se creo correctamente la noticia",
+          "success"
+        )
+      );
+    })
+  };
 
   useEffect(() => {
     searchNews();
@@ -61,16 +129,64 @@ const NewsScreen = () => {
           }}
         >
           <Card sx={{ maxWidth: 345 }}>
-            <CardHeader title="Nueva Noticia"/>
+            <CardHeader title="Nueva Noticia" />
             <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
+              <>
+                <FormControl fullWidth>
+                  <div
+                    style={{
+                      marginTop: "10px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <TextField
+                      id="standard-multiline-flexible"
+                      label="titulo"
+                      name="titulo"
+                      onChange={handleChangeSolution}
+                      variant="filled"
+                    />
+                  </div>
+                  <div
+                    style={{
+                      marginTop: "10px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <TextField
+                      id="standard-multiline-flexible"
+                      label="description"
+                      name="description"
+                      multiline
+                      maxRows={4}
+                      onChange={handleChangeSolution}
+                      variant="filled"
+                    />
+                  </div>
+                  <div
+                    style={{
+                      marginTop: "10px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <TextField
+                      id="standard-multiline-flexible"
+                      label="image"
+                      name="image"
+                      onChange={handleChangeSolution}
+                      variant="filled"
+                    />
+                  </div>
+                </FormControl>
+              </>
             </CardContent>
             <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
+              <Button size="small" onClick={handleClick}>
+                Crear
+              </Button>
             </CardActions>
           </Card>
         </div>
@@ -80,7 +196,7 @@ const NewsScreen = () => {
             justifyContent: "center",
           }}
         >
-          <Card sx={{ maxWidth: 345 }}>
+          {/* <Card sx={{ maxWidth: 345 }}>
             <CardMedia
               component="img"
               height="140"
@@ -96,7 +212,7 @@ const NewsScreen = () => {
                 6,000 species, ranging across all continents except Antarctica
               </Typography>
             </CardContent>
-          </Card>
+          </Card> */}
         </div>
       </div>
 
